cc47d187ba938b466928046678b45531ac487f45
nghuuphuoc <phuoc@huuphuoc.me>
2014-12-31 11:17:45 +0700

    Update uri validator document

--- _includes/validators/uri/html5-example.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#uriHtml5Form').bootstrapValidator({
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        }
-    });
-});
