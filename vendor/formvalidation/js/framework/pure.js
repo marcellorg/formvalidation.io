a8b42a7b5e7cf2d7fbbfde4da66eda9c28a7d18a
phuoc <phuoc@huuphuoc.me>
2015-01-04 21:15:00 +0700

    Add Pure stacked form usage

--- vendor/formvalidation/js/framework/pure.js
+++ vendor/formvalidation/js/framework/pure.js
@@ -2,7 +2,7 @@
  * FormValidation (http://formvalidation.io)
  * The best jQuery plugin to validate form fields. Support Bootstrap, Foundation, Pure, SemanticUI, UIKit frameworks
  *
- * @version     v0.6.0-dev, built on 2015-01-04 8:34:03 PM
+ * @version     v0.6.0-dev, built on 2015-01-04 9:13:55 PM
  * @author      https://twitter.com/nghuuphuoc
  * @copyright   (c) 2013 - 2015 Nguyen Huu Phuoc
  * @license     http://formvalidation.io/license/
@@ -33,8 +33,8 @@
             row: {
                 // http://purecss.io/forms/#aligned-form
                 selector: '.pure-control-group',
-                valid: '',
-                invalid: '',
+                valid: 'fv-has-success',
+                invalid: 'fv-has-error',
                 feedback: 'fv-has-feedback'
             }
         }, options);
