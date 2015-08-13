b765a4308a0669421657637dce6a8896a74f6366
nghuuphuoc <phuoc@huuphuoc.me>
2015-08-13 12:04:12 +0700

    Cleanup the example layouts

--- _includes/demo/foot.js
+++ _includes/demo/foot.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     $(window).on('load', function() {
         var $iframe = $(window.parent.document).find('iframe[src="/{{ page.path }}"]');
         if ($iframe.length) {
-            $('body').addClass('demo-frame-body').find('.demo-ad').hide();
+            //$('body').addClass('demo-frame-body').find('.demo-ad').hide();
 
             // Adjust the height of iframes containing the demo for specific frameworks
             var $container = $('#demoContainer');
